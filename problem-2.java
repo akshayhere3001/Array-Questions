e6b60f300d2348dc3fa69443876592f1cbd044a3
AKSHAY <87317537+akshayhere3001@users.noreply.github.com>
2022-07-22 11:17:47 +0530

    Create problem-2.java

---
+++ problem-2.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Problem-2{
+  public static void main(String[] args){
+      
+      	// Problem-2 -> Write a program to find if the given number is present in an Array or not
+        
+		int[] arr = {12, 1, 2, 3, 6, 5};
+		int num = 15;
+		boolean isInArray = false;
+		
+		// solving by For-Each loop
+		for(int element: arr) {
+			if(num == element) {
+				isInArray = true;
+				break;
+			}
+		}
+      
+      
+    }
+ }
