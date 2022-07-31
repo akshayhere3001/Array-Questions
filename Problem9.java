e93e07f32c73fb934ad397af341b80d2f37dc29a
AKSHAY <87317537+akshayhere3001@users.noreply.github.com>
2022-07-31 09:20:21 +0530

    Create Problem9.java

---
+++ Problem9.java
@@ -0,0 +1,21 @@
+public class Problem9{
+  public static void main(String[] args){
+  
+    // Problem-9 -> Finding Duplicate Elements from an Array
+    
+    // Array
+		int arr[] = {12,3,4,5,4,6, 6};
+    
+    // outer Loop to Iterate over an Array
+		for(int i = 0; i<arr.length; i++) {
+      // Inner Loop
+			for(int j = i + 1; j<arr.length; j++) {
+				if(arr[i] == arr[j]) {
+					System.out.println(arr[i]);
+				}
+			}
+		}
+    
+    
+  }
+}
